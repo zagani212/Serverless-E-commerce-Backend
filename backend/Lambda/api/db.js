688482a3b564832d5c184e38b4c551bc916a4102
zagani212 <abdelhakassahi@gmail.com>
2026-04-08 21:14:00 +0200

    rename functions

--- backend/Lambda/api/db.js
+++
@@ -1,7 +0,0 @@
-// db.js
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-
-const client = new DynamoDBClient({ region: "eu-west-3" });
-
-export const db = DynamoDBDocumentClient.from(client);
