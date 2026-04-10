ae1ce8a1ba329510894f6632bce8522586b2954a
zagani212 <abdelhakassahi@gmail.com>
2026-04-10 19:24:20 +0200

    add templates folder

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sam deploy --template-file s3.yaml --stack-name s3-stack --no-confirm-changeset
-sam deploy --template-file dynamodb.yaml --stack-name dynamodb-stack --no-confirm-changeset
+sam deploy --template-file templates/s3.yaml --stack-name s3-stack --no-confirm-changeset
+sam deploy --template-file templates/dynamodb.yaml --stack-name dynamodb-stack --no-confirm-changeset
 
 sam deploy \
-    --template-file amplify.yaml \
+    --template-file templates/amplify.yaml \
     --stack-name amplify-stack \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
@@ -12,8 +12,8 @@ sam deploy \
     GitHubRepo=YOUR_ITHUB_REPO \
     --no-confirm-changeset
 
-sam deploy --template-file cognito.yaml --stack-name cognito-stack --no-confirm-changeset
-sam deploy --template-file functions.yaml --stack-name lambda-stack --no-confirm-changeset
-sam deploy --template-file stepFunctions.yaml --stack-name stepFunctions-stack --capabilities CAPABILITY_NAMED_IAM --no-confirm-changeset
-sam deploy --template-file api-gateway.yaml --stack-name api-stack --no-confirm-changeset
-sam deploy --template-file amplify-branch.yaml --stack-name amplify-branch-stack --no-confirm-changeset
+sam deploy --template-file templates/cognito.yaml --stack-name cognito-stack --no-confirm-changeset
+sam deploy --template-file templates/functions.yaml --stack-name lambda-stack --no-confirm-changeset
+sam deploy --template-file templates/stepFunctions.yaml --stack-name stepFunctions-stack --capabilities CAPABILITY_NAMED_IAM --no-confirm-changeset
+sam deploy --template-file templates/api-gateway.yaml --stack-name api-stack --no-confirm-changeset
+sam deploy --template-file templates/amplify-branch.yaml --stack-name amplify-branch-stack --no-confirm-changeset
