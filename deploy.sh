7abe499cb41e56f1a43a1cea870ed03d576b93d0
zagani212 <abdelhakassahi@gmail.com>
2026-04-13 21:12:13 +0200

    add some important options to aws sam

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sam deploy --template-file templates/s3.yaml --stack-name s3-stack --guided
-sam deploy --template-file templates/dynamodb.yaml --stack-name dynamodb-stack --guided
+sam deploy --template-file templates/s3.yaml --stack-name s3-stack --no-confirm-changeset
+sam deploy --template-file templates/dynamodb.yaml --stack-name dynamodb-stack --no-confirm-changeset
 
 sam deploy \
     --template-file templates/amplify.yaml \
     --stack-name amplify-stack \
-    --capabilities CAPABILITY_NAMED_IAM \
+    --capabilities CAPABILITY_IAM \
     --parameter-overrides \
     GitHubToken=YOUR_GITHUB_TOKEN \
-    GitHubRepo=https://github.com/zagani212/amplify_app.git
+    GitHubRepo=https://github.com/zagani212/amplify_app.git --no-confirm-changeset
 
-sam deploy --template-file templates/cognito.yaml --stack-name cognito-stack --guided
-sam deploy --template-file templates/functions.yaml --stack-name lambda-stack --guided
-sam deploy --template-file templates/stepFunctions.yaml --stack-name stepFunctions-stack --capabilities CAPABILITY_NAMED_IAM --guided
-sam deploy --template-file templates/api-gateway.yaml --stack-name api-stack --guided
-sam deploy --template-file templates/amplify-branch.yaml --stack-name amplify-branch-stack --guided
+sam deploy --template-file templates/cognito.yaml --stack-name cognito-stack --capabilities CAPABILITY_IAM --no-confirm-changeset --resolve-s3
+sam deploy --template-file templates/functions.yaml --stack-name lambda-stack --capabilities CAPABILITY_IAM --no-confirm-changeset --resolve-s3
+sam deploy --template-file templates/stepFunctions.yaml --stack-name stepFunctions-stack --capabilities CAPABILITY_IAM --no-confirm-changeset --resolve-s3
+sam deploy --template-file templates/api-gateway.yaml --stack-name api-stack --no-confirm-changeset
+sam deploy --template-file templates/amplify-branch.yaml --stack-name amplify-branch-stack --no-confirm-changeset
